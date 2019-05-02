99516cd657060a074dfdd13747686dff338da132
Ping Chan Chua <ping.chan.chua@autodesk.com>
2019-05-03 02:23:08 +0800

    Add design to front end.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
+import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/*
         <FaceRecognition />
