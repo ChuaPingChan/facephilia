bc47d608962ca47029b4f196cf1fa3fedc07b597
Ping Chan Chua <ping.chan.chua@autodesk.com>
2019-05-02 23:20:40 +0800

    Add logo and image link form(empty).

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-        {/*
         <Logo />
         <ImageLinkForm />
+        {/*
         <FaceRecognition />
         */}
       </div>
