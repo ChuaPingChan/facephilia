3bad0215e57b0841c49c2f8a02da82234c597a78
Ping Chan Chua <pingchan94@gmail.com>
2019-05-12 03:41:31 +0800

    Make rank number working by implementing /image route.

--- src/App.js
+++ src/App.js
@@ -78,10 +78,25 @@ class App extends React.Component {
     this.setState({input: event.target.value});
   }
 
-  onButtonSubmit = () => {
+  onPictureSubmit = () => {
     this.setState({imageUrl: this.state.input});
     app.models.predict("a403429f2ddf4b49b307e318f00e528b", this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3001/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, {entries: count}))
+            })
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err => console.log(err));
   }
 
@@ -109,7 +124,7 @@ class App extends React.Component {
           ? <div>
               <Logo />
               <Rank name={this.state.user.name} entries={this.state.user.entries}/>
-              <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+              <ImageLinkForm onInputChange={this.onInputChange} onPictureSubmit={this.onPictureSubmit}/>
               <FaceRecognition box={box} imageUrl={imageUrl}/>
             </div>
           : ( route === 'register'
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ const Rank = ({ name, entries }) => {
     return (
         <div>
             <div className='white f3'>
-                {`Hi ${name}, number of images processed`}
+                {`Hi ${name}, your current entry count is`}
             </div>
             <div className='white f1'>
                 {entries}
