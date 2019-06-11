f8cb88a637724dbda172a8e7b3308e053cba1143
Ping Chan Chua <ping.chan.chua@autodesk.com>
2019-06-11 23:23:32 +0800

    Updates

---
+++ precache-manifest.04d98a1215e315603b3e15e77eef0e4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "171cb86736bb784c47ce25869672d24f",
+    "url": "/facephilia/index.html"
+  },
+  {
+    "revision": "86ba1a9d2b5b1bc023df",
+    "url": "/facephilia/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4e75eda9f5f5a652ecc2",
+    "url": "/facephilia/static/css/main.77f6329e.chunk.css"
+  },
+  {
+    "revision": "86ba1a9d2b5b1bc023df",
+    "url": "/facephilia/static/js/2.7ca30bdf.chunk.js"
+  },
+  {
+    "revision": "4e75eda9f5f5a652ecc2",
+    "url": "/facephilia/static/js/main.fcaae9bf.chunk.js"
+  },
+  {
+    "revision": "ec7d52e0f249fc8d1136",
+    "url": "/facephilia/static/js/runtime~main.d86f32bd.js"
+  }
+]);
